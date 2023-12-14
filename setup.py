f3c1e7f141af6faafa4cf2b73adc2cef504a1bde
rushin236 <rushikeshshindesandesh@gmail.com>
2023-12-15 02:24:32 +0530

    dvc init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its is a wafer projects with mlops',
+    author='rushin236',
+    license='MIT',
+)
